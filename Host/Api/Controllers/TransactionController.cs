93a430e3ee55f6daa58bc8a5340ddfafe2a7d66e
Daniel Baccarin <danielbaccarin@outlook.com>
2026-01-29 20:02:17 -0300

    Implementing Api project with deploy in GCP Cloud Run.

---
+++ Host/Api/Controllers/TransactionController.cs
@@ -0,0 +1,25 @@
+﻿using Api.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransactionController : Controller
+    {
+        [HttpGet("Get")]
+        public IEnumerable<Transaction> Get()
+        {
+            return new List<Transaction>()
+            {
+                new Transaction()
+                {
+                    Id = new Guid(),
+                    Description = "Test",
+                    Value = 10,
+                    Date = DateTime.Now
+                }
+            };
+        }
+    }
+}
---
+++ Host/Api/Model/Transaction.cs
@@ -0,0 +1,13 @@
+﻿namespace Api.Model
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+
+        public string Description { get; set; }
+
+        public double Value { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
